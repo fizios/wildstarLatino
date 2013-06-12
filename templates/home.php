82aea43b8ab0074ca53d00489ee97bcb0a6963c1
fizios <marianonc.84@gmail.com>
2013-06-12 11:43:50 -0300

    modificacion iframe fb

--- templates/home.php
+++ templates/home.php
@@ -40,6 +40,7 @@ $css_array[] = "home.css";
 	              	href="https://twitter.com/WildstarLatino" data-widget-id="341293685570404353">Tweets by @WildstarLatino</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
+				
             	<div class="facebook-container">
               		<div class="fb-like-box" data-href="https://www.facebook.com/WildstarLatino" data-width="270" data-show-faces="true" data-colorscheme="" data-stream="false" data-show-border="false" data-header="true"></div>
              	</div>
