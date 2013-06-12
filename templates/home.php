54db87addc5d659d3e05be6e779aca8c66bdd820
fizios <marianonc.84@gmail.com>
2013-06-12 11:06:23 -0300

    modificacion iframe fb

--- templates/home.php
+++ templates/home.php
@@ -35,14 +35,14 @@ $css_array[] = "home.css";
 	            <div class="beta-banner">
 		        	<a href="http://www.wildstar-online.com/en/beta/"><img src="/imagenes/beta-banner.png" class="pull-right img-rounded" width="100%"></a>
 	            </div>
-            	<div class="facebook-container">
-              		<div class="fb-like-box" data-href="https://www.facebook.com/WildstarLatino" data-width="270" data-show-faces="true" data-colorscheme="" data-stream="false" data-show-border="false" data-header="true"></div>
-             	</div>
               	<div class="twitter-container">
 	              	<a class="twitter-timeline" width="270" height="300" data-link-color="#0084b4" data-theme="light"
 	              	href="https://twitter.com/WildstarLatino" data-widget-id="341293685570404353">Tweets by @WildstarLatino</a>
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
+            	<div class="facebook-container">
+              		<div class="fb-like-box" data-href="https://www.facebook.com/WildstarLatino" data-width="270" data-show-faces="true" data-colorscheme="" data-stream="false" data-show-border="false" data-header="true"></div>
+             	</div>
             </div>
           </div>
         </div>
