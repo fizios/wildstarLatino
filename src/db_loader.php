f101e3ec1943570b4976e0ceb6a554b91c98796b
fizios <marianonc.84@gmail.com>
2013-07-19 21:50:13 -0300

    db loader

--- src/db_loader.php
+++ src/db_loader.php
@@ -2,8 +2,8 @@
 include $_SERVER['DOCUMENT_ROOT'].'/app/ezSQL/shared/ez_sql_core.php';
 include $_SERVER['DOCUMENT_ROOT'].'/app/ezSQL/mysql/ez_sql_mysql.php';
 
-$db_user = 'root';
-$db_password = '';
+$db_user = 'wildstar';
+$db_password = 'Fizios84';
 $db_name = 'wildstar_wildstardb';
 $db_host = 'localhost';
 
